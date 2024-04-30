5ce9f28293e21fcb26215ea8c30ec6bd9f59099c
Omid <166237836+Rozogar@users.noreply.github.com>
2024-04-30 00:04:30 -0700

    setting the icon of the application, prepated the splash page and designed the Main page

--- app/src/main/java/com/rozogar/hazaragi/ui/theme/SplashPage.java
+++ app/src/main/java/com/rozogar/hazaragi/ui/theme/SplashPage.java
@@ -2,15 +2,27 @@ package com.rozogar.hazaragi.ui.theme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.myapplication.R;
 
 public class SplashPage extends AppCompatActivity {
-
+Handler handler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_page);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashPage.this, Main.class);
+                startActivity(intent);
+                finish();
+
+            }
+        }, 3000);
+
     }
 }
