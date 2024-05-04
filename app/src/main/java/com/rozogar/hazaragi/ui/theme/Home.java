a4b5a0093ed94075634f4342d8b8439fd870e7fe
Omid <166237836+Rozogar@users.noreply.github.com>
2024-05-04 11:02:15 -0700

    Few changes like adding some new songs and set the number for each songs. 1403/2/15

--- app/src/main/java/com/rozogar/hazaragi/ui/theme/Home.java
+++ app/src/main/java/com/rozogar/hazaragi/ui/theme/Home.java
@@ -36,30 +36,31 @@ private ArrayList<Music> m;
 
     private void filling() {
         m = new ArrayList<>();
-        m.add(new Music( R.drawable.a,R.raw.khantoma,"خنتوما"));
-        m.add(new Music( R.drawable.ger,R.raw.mix,"میکس هزارگی"));
-        m.add(new Music( R.drawable.fone,R.raw.awal,"احوال بند دل"));
-        m.add(new Music( R.drawable.hi,R.raw.anaranar,"انار انار"));
-        m.add(new Music( R.drawable.j,R.raw.delbar,"دلبر"));
-        m.add(new Music( R.drawable.e,R.raw.azsharbaqarya,"از شار به قریه"));
-        m.add(new Music( R.drawable.n,R.raw.khandida,"خندیده میآیی"));
-        m.add(new Music( R.drawable.i,R.raw.daraeehazara,"دره هزاره"));
-        m.add(new Music( R.drawable.m,R.raw.golandam,"گل اندام"));
-        m.add(new Music( R.drawable.dd,R.raw.zendagi,"زندگی"));
-        m.add(new Music( R.drawable.p,R.raw.remix1,"میکس 1"));
-        m.add(new Music( R.drawable.g,R.raw.bashkinarm,"کنارم باش"));
-        m.add(new Music( R.drawable.k,R.raw.dokhtaraknaz,"دخترک ناز"));
-        m.add(new Music( R.drawable.h,R.raw.chagarma,"چگرمه"));
-        m.add(new Music( R.drawable.r,R.raw.parizad,"پریزاد "));
-        m.add(new Music( R.drawable.s,R.raw.safidi,"سفیدی مخملی"));
-        m.add(new Music( R.drawable.b,R.raw.khoshnoma,"خوش نمایی"));
-        m.add(new Music( R.drawable.q,R.raw.remix2,"میکس 2"));
-        m.add(new Music( R.drawable.f,R.raw.balidel,"بلا دل"));
-        m.add(new Music( R.drawable.aa,R.raw.sharabjan,"میده میده"));
-        m.add(new Music( R.drawable.l,R.raw.eshqiman,"عشق من"));
-        m.add(new Music( R.drawable.cute,R.raw.dokhtarihazara,"دخت هزاره"));
-        m.add(new Music( R.drawable.o,R.raw.khyalto,"خیال تو"));
-        m.add(new Music( R.drawable.cc,R.raw.tokimaeee,"تو کی میآیی"));
+        m.add(new Music( R.drawable.a,R.raw.khantoma,"1- خنتوما"));
+        m.add(new Music( R.drawable.ger,R.raw.mix,"2- میکس هزارگی"));
+        m.add(new Music( R.drawable.fone,R.raw.jorajan,"3- جوره جانیم نموشی"));
+        m.add(new Music( R.drawable.hi,R.raw.anaranar,"4- انار انار"));
+        m.add(new Music( R.drawable.j,R.raw.delbar,"5- دلبر"));
+        m.add(new Music( R.drawable.e,R.raw.azsharbaqarya,"6- از شار به قریه"));
+        m.add(new Music( R.drawable.n,R.raw.khandida,"7- خندیده میآیی"));
+        m.add(new Music( R.drawable.i,R.raw.daraeehazara,"8- دره هزاره"));
+        m.add(new Music( R.drawable.m,R.raw.golandam,"9- گل اندام"));
+        m.add(new Music( R.drawable.dd,R.raw.zendagi,"10- زندگی"));
+        m.add(new Music( R.drawable.p,R.raw.remix1,"11- میکس 1"));
+        m.add(new Music( R.drawable.g,R.raw.bashkinarm,"12- کنارم باش"));
+        m.add(new Music( R.drawable.k,R.raw.dokhtaraknaz,"13- دخترک ناز"));
+        m.add(new Music( R.drawable.h,R.raw.chagarma,"14- چگرمه"));
+        m.add(new Music( R.drawable.r,R.raw.parizad,"15- پریزاد "));
+        m.add(new Music( R.drawable.s,R.raw.safidi,"16- سفیدی مخملی"));
+        m.add(new Music( R.drawable.b,R.raw.khoshnoma,"17- خوش نمایی"));
+        m.add(new Music( R.drawable.q,R.raw.remix2,"18- میکس 2"));
+        m.add(new Music( R.drawable.f,R.raw.balidel,"19- بلا دل"));
+        m.add(new Music( R.drawable.aa,R.raw.sharabjan,"20- میده میده"));
+        m.add(new Music( R.drawable.l,R.raw.eshqiman,"21- عشق من"));
+        m.add(new Music( R.drawable.cute,R.raw.dokhtarihazara,"22- دخت هزاره"));
+        m.add(new Music( R.drawable.o,R.raw.remix3,"23- میکس 3"));
+        m.add(new Music( R.drawable.cc,R.raw.tokimaeee,"24- تو کی میآیی"));
+        m.add(new Music( R.drawable.awal,R.raw.awal,"25- احوال بند دل"));
 
 
 
